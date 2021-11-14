219e7382ef5a833930d7004883dde603ba440738
konnenhannaford <konnen23@gmail.com>
2021-11-14 12:27:46 +1100

    table issue

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,29 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR(30) NOT NULL
 );
 CREATE TABLE roles (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    title VARCHAR(30) NOT NULL,
    salary DECIMAL,
    department_id INT,
-   FOREIGN KEY 
-   REFERENCES
+   FOREIGN KEY (department_id)
+   REFERENCES department(id)
+   ON DELETE SET NULL
 );
 CREATE TABLE employee (
-   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
    first_name VARCHAR(30) NOT NULL,
    last_name VARCHAR(30) NOT NULL,
    role_id INT,
    manager_id INT,
    FOREIGN KEY (role_id)
    REFERENCES roles(id)
-    ON DELETE SET NULL
-   
+   ON DELETE SET NULL,
    FOREIGN KEY (manager_id)
-   REFERENCES roles(id)
-    ON DELETE SET NULL
+   REFERENCES employee(id)
+   ON DELETE SET NULL
 
 );
