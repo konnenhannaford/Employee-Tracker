106c76069cfdf18078d7ba07274506fda1e07839
konnenhannaford <konnen23@gmail.com>
2021-11-15 00:56:14 +1100

    successfully created tables and input info

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,33 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+--    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    name VARCHAR(30) NOT NULL
 );
 CREATE TABLE roles (
-   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   id INT AUTO_INCREMENT NOT NULL,
    title VARCHAR(30) NOT NULL,
    salary DECIMAL,
    department_id INT,
-   FOREIGN KEY (department_id)
-   REFERENCES department(id)
-   ON DELETE SET NULL
+   PRIMARY KEY (id)
+--    FOREIGN KEY (department_id)
+--    REFERENCES department(id)
+--    ON DELETE SET NULL
 );
 CREATE TABLE employees (
-   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+   id INT AUTO_INCREMENT NOT NULL,
    first_name VARCHAR(30) NOT NULL,
    last_name VARCHAR(30) NOT NULL,
    role_id INT,
    manager_id INT,
-   FOREIGN KEY (role_id)
-   REFERENCES roles(id)
-   ON DELETE SET NULL,
-   FOREIGN KEY (manager_id)
-   REFERENCES employee(id)
-   ON DELETE SET NULL
+   PRIMARY KEY (id)
+
+--    FOREIGN KEY (role_id)
+--    REFERENCES roles(id)
+--    ON DELETE SET NULL,
+--    FOREIGN KEY (manager_id)
+--    REFERENCES employee(id)
+--    ON DELETE SET NULL
 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO department ( name )
 VALUES
     ( "Customer Service" ),
-    -- 2
+    -- 1
     ( "Human Resources" ),
     -- 2
     ( "Marketing" ),
@@ -16,79 +16,69 @@ SELECT * FROM department;
 INSERT INTO roles ( title, salary, department_id )
 VALUES
 -- Customer Service
-("phone operator", "50000", 1),
-("phone operator", "50000", 1),
-("customer relations ambassador", "55000", 1),
-("customer relations ambassador", "55000", 1),
-("complaints manager", "60000", 1),
-("complaints manager", "60000", 1),
+("phone operator", 50000, 1),
+("customer relations ambassador", 55000, 1),
+("complaints manager", 60000, 1),
 -- Human Resources
-("internal disputes", "50000", 2),
-("internal disputes", "50000", 2),
-("hiring manager", "55000", 2),
-("hiring manager", "55000", 2),
-("legal", "60000" 2),
-("legal", "60000" 2),
+("internal disputes", 50000, 2),
+("hiring manager", 55000, 2),
+("legal", 60000, 2),
 -- Marketing
-("social media manager", "50000", 3),
-("social media manager", "50000", 3),
-("graphic designer", "55000", 3),
-("graphic designer", "55000", 3),
-("sponsorship coordinator", "60000", 3),
-("sponsorship coordinator", "60000", 3),
+("social media manager", 50000, 3),
+("graphic designer", 55000, 3),
+("sponsorship coordinator", 60000, 3),
 -- Traders
-("pre-game trader", "50000", 4),
-("pre-game trader", "50000", 4),
-("in-game trader", "55000", 4),
-("in-game trader", "55000", 4),
-("trading control", "60000", 4),
-("trading control", "60000", 4),
+("pre-game trader", 50000, 4),
+("in-game trader", 55000, 4),
+("trading control", 60000, 4),
 -- Senior Management
-("customer service manager", "70000", 5),
-("hr manager", "70000", 5),
-("marketing manager", "70000", 5),
-("trading manager", "70000", 5),
-("COO", "80000", 5),
-("CEO", "90000", 5);
+("customer service manager", 70000, 5),
+("hr manager", 70000, 5),
+("marketing manager", 70000, 5),
+("trading manager", 70000, 5),
+("COO", 80000, 5),
+("CEO", 90000, 5);
 
 
+SELECT * FROM roles;
 
 
-INSERT INTO employee ( first_name, last_name, role_id, manager_id )
+INSERT INTO employeesdata ( first_name, last_name, role_id, manager_id )
 VALUES
-    ("Bernd", "Leno", 1),
-    ("Kieran", "Tierney", 2),
-    ("Ben", "White", 3),
-    ("Thomas", "Partey", 4 ),
-    ("Gabriel", "Magalhaes", 5),
-    ("Bukayo", "Saka", 6),
+    ("Bernd", "Leno", 1, 13),
+    ("Kieran", "Tierney", 1, 13),
+    ("Ben", "White", 2, 13),
+    ("Thomas", "Partey", 2, 13),
+    ("Gabriel", "Magalhaes", 3, 13),
+    ("Bukayo", "Saka", 3, 13),
 
-    ("Martin", "Odegaard", 7),
-    ("Alex", "Lacazette", 8),
-    ("Emile", "Smith-Rowe", 9),
-    ("Pierre", "Aubameyang", 10),
-    ("Ainsley", "Maitland-Niles", 11),
-    ("Rob", "Holding", 12),
+    ("Martin", "Odegaard", 4, 14),
+    ("Alex", "Lacazette", 4, 14),
+    ("Emile", "Smith-Rowe", 5, 14),
+    ("Pierre", "Aubameyang", 5, 14),
+    ("Ainsley", "Maitland-Niles", 6, 14),
+    ("Rob", "Holding", 6, 14),
 
-    ("Cedric", "Soares", 13),
-    ("Takehiro", "Tomiyasu", 14 ),
-    ("Nicolas", "Pepe", 15),
-    ("Nuno", "Tavares", 16),
-    ("Calum", "Chambers", 17),
-    ("Pablo", "Mari", 18),
+    ("Cedric", "Soares", 7, 15),
+    ("Takehiro", "Tomiyasu", 7, 15 ),
+    ("Nicolas", "Pepe", 8, 15),
+    ("Nuno", "Tavares", 8, 15),
+    ("Calum", "Chambers", 9, 15),
+    ("Pablo", "Mari", 9, 15,
 
-    ("Albert", "Sambi-Lokonga", 19),
-    ("Mohammed", "Elneny", 20),
-    ("Folarin", "Balogun", 21),
-    ("Eddie", "Nketiah", 22),
-    ("Sead", "Kolasinac", 23),
-    ("Aaron", "Ramsdale", 24 ),
+    ("Albert", "Sambi-Lokonga", 10, 16),
+    ("Mohammed", "Elneny", 10, 16),
+    ("Folarin", "Balogun", 11, 16),
+    ("Eddie", "Nketiah", 11, 16),
+    ("Sead", "Kolasinac", 12, 16),
+    ("Aaron", "Ramsdale", 12, 16),
 
-    ("Arthur", "Okonkwo", 25, NULL),
-    ("Granit", "Xhaka", 26, NULL),
-    ("Gabriel", "Martinelli", 27, NULL),
-    ("William", "Saliba", 28, NULL),
-    ("Reiss", "Nelson", 29, NULL),
-    ("Konnen", "Hannaford", 30, NULL);
+    ("Arthur", "Okonkwo", 13, NULL),
+    ("Granit", "Xhaka", 14, NULL),
+    ("Gabriel", "Martinelli", 15, NULL),
+    ("William", "Saliba", 16, NULL),
+    ("Reiss", "Nelson", 17, NULL),
+    ("Konnen", "Hannaford", 18, NULL);
     -- UPDATE employee SET manager_id=9 WHERE id!=9;
 
+SELECT * FROM employeesdata;
