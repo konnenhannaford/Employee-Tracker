a980609c9a33ca16ddd1a9ce85296bf64fe5f8cb
konnenhannaford <konnen23@gmail.com>
2021-11-18 11:58:47 +1100

    Remove extra code on idnex.js

--- index.js
+++ index.js
@@ -13,189 +13,189 @@ const db = mysql.createConnection(
     console.log(`Connected to the employeetracker_db`)
   );
   
-// userPrompts = () => {
-//     inquirer.prompt({
-//         name: "Employee Database - Begin",
-//         type: "list",
-//         message: "Thanks for using the employee tracker, follow promopts to review and update",
-//         choices:[
-//         "View Departments", 
-//         "View  Roles", 
-//         "View  Employees", 
-//         "Add a Department", 
-//         "Add a Role", 
-//         "Add an Employee", 
-//         "Update and Existing Employee"],
+// // userPrompts = () => {
+// //     inquirer.prompt({
+// //         name: "Employee Database - Begin",
+// //         type: "list",
+// //         message: "Thanks for using the employee tracker, follow promopts to review and update",
+// //         choices:[
+// //         "View Departments", 
+// //         "View  Roles", 
+// //         "View  Employees", 
+// //         "Add a Department", 
+// //         "Add a Role", 
+// //         "Add an Employee", 
+// //         "Update and Existing Employee"],
 
-    // }).then((choices) => {
-    //     console.log (choices)
-    //     if(choices.userChoice === "View  Departments"){
-    //         viewDepartments();
-    //     } else if (choices.userChoice === "View Roles"){
-    //         viewRoles();
-    //     } else if (choices.userChoice === "View Employees"){
-    //         viewEmployees();
-    //     } else if (choices.userChoice === "Add Department"){
-    //         addDepartment();
-    //     } else if (choices.userChoice === "Add role"){
-    //         addRole();
-    //     } else if (choices.userChoice === "Add Employee"){
-    //         addEmployee();
-    //     } else if (choices.userChoice === "Modify Employee"){
-    //         updateEmployee();
-    //     }
-        // // bonus sect
-        // else if (choices.userChoice === "View Employees by Manager"){
-        //     // viewEbyM();
-        // } else if (choices.userChoice === "View Employees by Department"){
-        //     // viewEbyD();
-        // } else if (choices.userChoice === "Delete a Department"){
-        //     // DeleteDepartment();
-        // } else if (choices.userChoice === "Delete a Role"){
-        //     // DeleteRole();
-        // } else if (choices.userChoice === "Delete an Employee"){
-        //     // DeleteEmployee();
-        // } else if (choices.userChoice === "Modify Employee Manager"){
-        //     // updateEmployeeManager();
-        // }
-//     })
-// };
+//     // }).then((choices) => {
+//     //     console.log (choices)
+//     //     if(choices.userChoice === "View  Departments"){
+//     //         viewDepartments();
+//     //     } else if (choices.userChoice === "View Roles"){
+//     //         viewRoles();
+//     //     } else if (choices.userChoice === "View Employees"){
+//     //         viewEmployees();
+//     //     } else if (choices.userChoice === "Add Department"){
+//     //         addDepartment();
+//     //     } else if (choices.userChoice === "Add role"){
+//     //         addRole();
+//     //     } else if (choices.userChoice === "Add Employee"){
+//     //         addEmployee();
+//     //     } else if (choices.userChoice === "Modify Employee"){
+//     //         updateEmployee();
+//     //     }
+//         // // bonus sect
+//         // else if (choices.userChoice === "View Employees by Manager"){
+//         //     // viewEbyM();
+//         // } else if (choices.userChoice === "View Employees by Department"){
+//         //     // viewEbyD();
+//         // } else if (choices.userChoice === "Delete a Department"){
+//         //     // DeleteDepartment();
+//         // } else if (choices.userChoice === "Delete a Role"){
+//         //     // DeleteRole();
+//         // } else if (choices.userChoice === "Delete an Employee"){
+//         //     // DeleteEmployee();
+//         // } else if (choices.userChoice === "Modify Employee Manager"){
+//         //     // updateEmployeeManager();
+//         // }
+// //     })
+// // };
 
-// function init () {
-//     console.log(`Follow prompts to input data`)
+// // function init () {
+// //     console.log(`Follow prompts to input data`)
 
-// userPrompts();
-// };
+// // userPrompts();
+// // };
 
-// // Deps
-// viewDepartment = () => {
-//     db.query("Select * from departments") 
-//         .then (data => {
-//             console.table(data)
-//         })
-// }
+// // // Deps
+// // viewDepartment = () => {
+// //     db.query("Select * from departments") 
+// //         .then (data => {
+// //             console.table(data)
+// //         })
+// // }
 
-// // roles
-// viewRoles = () => {
-//     db.query("Select * from roles") 
-//         .then (data => {
-//             console.table(data)
-//         })
-// }
-// // employee
-// viewEmployeesData = () => {
-//     db.query("Select * from employeesdata") 
-//         .then (data => {
-//             console.table(data)
-//         })
-// }
+// // // roles
+// // viewRoles = () => {
+// //     db.query("Select * from roles") 
+// //         .then (data => {
+// //             console.table(data)
+// //         })
+// // }
+// // // employee
+// // viewEmployeesData = () => {
+// //     db.query("Select * from employeesdata") 
+// //         .then (data => {
+// //             console.table(data)
+// //         })
+// // }
 
-// addDepartment = () => {
+// // addDepartment = () => {
 
-//     const addDepartment = [
-//         {
-//             type:"text",
-//             message:"Add Department",
-//             name: "departmentAdd"
-//         },
-//         {
-//             type:"text",
-//             message:"Add Department ID?",
-//             name: "departmentID"
-//         }
-//     ];
-//     prompt(addDepartment)
-//     .then((department)=> {
-//         db.createDepartment(department)
-//         .then(() => {
-//             console.log(`Add Department: ${department}`)
-//         })
-//         .then(() => {
-//             userPrompts();
-//         })
-//     })
+// //     const addDepartment = [
+// //         {
+// //             type:"text",
+// //             message:"Add Department",
+// //             name: "departmentAdd"
+// //         },
+// //         {
+// //             type:"text",
+// //             message:"Add Department ID?",
+// //             name: "departmentID"
+// //         }
+// //     ];
+// //     prompt(addDepartment)
+// //     .then((department)=> {
+// //         db.createDepartment(department)
+// //         .then(() => {
+// //             console.log(`Add Department: ${department}`)
+// //         })
+// //         .then(() => {
+// //             userPrompts();
+// //         })
+// //     })
 
-// }
+// // }
 
-// addRole = () => {
+// // addRole = () => {
 
-//     const addRole = [
-//         {
-//             type:"text",
-//             message:"Add role ID",
-//             name: "roleID"
-//         },
-//         {
-//             type:"text",
-//             message:"Add Job role title",
-//             name: "roleTitle"
-//         },
-//         {
-//             type:"text",
-//             message:"Add salary",
-//             name: "roleSalary"
-//         },
-//     ]
-//     prompt(addRole)
-//     .then((role)=> {
-//         db.createRole(role)
-//         .then(() => {
-//             console.log(`Add role: ${role}`)
-//         })
-//         .then(() => {
-//             userPrompts();
-//         });
-//     });
+// //     const addRole = [
+// //         {
+// //             type:"text",
+// //             message:"Add role ID",
+// //             name: "roleID"
+// //         },
+// //         {
+// //             type:"text",
+// //             message:"Add Job role title",
+// //             name: "roleTitle"
+// //         },
+// //         {
+// //             type:"text",
+// //             message:"Add salary",
+// //             name: "roleSalary"
+// //         },
+// //     ]
+// //     prompt(addRole)
+// //     .then((role)=> {
+// //         db.createRole(role)
+// //         .then(() => {
+// //             console.log(`Add role: ${role}`)
+// //         })
+// //         .then(() => {
+// //             userPrompts();
+// //         });
+// //     });
     
-// }
+// // }
 
-// addEmployee = () => {
+// // addEmployee = () => {
 
-//     const addEmployee = [
-//         {
-//             name: "employeeID",
-//             type:"text",
-//             message:"What is the ID for the employee?"
+// //     const addEmployee = [
+// //         {
+// //             name: "employeeID",
+// //             type:"text",
+// //             message:"What is the ID for the employee?"
             
-//         },
-//         {
-//             name: "employeeFirstName",
-//             type:"text",
-//             message:"Whats the employees name (first)?"
+// //         },
+// //         {
+// //             name: "employeeFirstName",
+// //             type:"text",
+// //             message:"Whats the employees name (first)?"
             
-//         },
-//         {
-//             name: "employeeLastName",
-//             type:"text",
-//             message:"Whats the employees name (last)?"
+// //         },
+// //         {
+// //             name: "employeeLastName",
+// //             type:"text",
+// //             message:"Whats the employees name (last)?"
            
-//         },
-//         {
-//             name: "employeeRoleID",
-//             type:"text",
-//             message:"What is the ID for this employee?"
+// //         },
+// //         {
+// //             name: "employeeRoleID",
+// //             type:"text",
+// //             message:"What is the ID for this employee?"
             
-//         },
-//         {
-//             name: "employeeManagerID",
-//             type:"text",
-//             message:"What is the Manager ID for this employee?"
+// //         },
+// //         {
+// //             name: "employeeManagerID",
+// //             type:"text",
+// //             message:"What is the Manager ID for this employee?"
             
-//         }
-//     ]
-//     prompt(addEmployee)
-//     .then((employee)=> {
-//         db.createRole(employee)
-//         .then(() => {
-//             console.log(`Add Employee - ${employee}`)
-//         })
-//         .then(() => {
-//             userPrompts();
-//         });
-//     });
-// }
+// //         }
+// //     ]
+// //     prompt(addEmployee)
+// //     .then((employee)=> {
+// //         db.createRole(employee)
+// //         .then(() => {
+// //             console.log(`Add Employee - ${employee}`)
+// //         })
+// //         .then(() => {
+// //             userPrompts();
+// //         });
+// //     });
+// // }
 
-// init();
+// // init();
 viewDepartment();
 
 function viewDepartment() {
@@ -219,3 +219,7 @@ function viewRoles() {
         console.log(res)
     });
 };
+
+
+
+
